76cc2c2c9eb55640eb96c2115e12f24292ec0cd8
Julio Marquez <juliocmrqzm@gmail.com>
2017-01-21 18:01:01 -0400

    Restrict the API access only to admin users

--- config/settings/common.py
+++ config/settings/common.py
@@ -278,7 +278,7 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.BrowsableAPIRenderer',
     ),
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAdminUser',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
