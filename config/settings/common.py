babba5e9d0b0ec5702ca57519ec7c4f133d24bf6
Julio Marquez <juliocmrqzm@gmail.com>
2017-01-21 18:03:01 -0400

    Set the date format of the API to 'iso-8601'

--- config/settings/common.py
+++ config/settings/common.py
@@ -272,7 +272,6 @@ ADMIN_URL = r'^admin/'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': env.int('DJANGO_PAGINATION_LIMIT', 10),
-    'DATETIME_FORMAT': '%Y-%m-%dT%H:%M:%S%z',
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
