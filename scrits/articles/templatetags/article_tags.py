5cb57d2d53b5f303b6a0b791e86a9feeec99539d
Julio Marquez <juliocmrqzm@gmail.com>
2017-02-08 18:17:12 -0400

    Create a new template tag: recent_articles

--- scrits/articles/templatetags/article_tags.py
+++ scrits/articles/templatetags/article_tags.py
@@ -2,6 +2,8 @@
 from django.template import Library
 from django.utils.safestring import mark_safe
 
+from ..models import Article
+
 from ...base.tools import markdown_to_html
 
 register = Library()
@@ -10,3 +12,11 @@ register = Library()
 @register.filter
 def apply_markdown(text):
     return markdown_to_html(text)
+
+
+@register.inclusion_tag('inclusion_tags/recent_articles.html')
+def recent_articles():
+    articles = Article.objects.all().published()
+    articles = articles.order_by('-created')
+
+    return {'articles': articles[:5]}
